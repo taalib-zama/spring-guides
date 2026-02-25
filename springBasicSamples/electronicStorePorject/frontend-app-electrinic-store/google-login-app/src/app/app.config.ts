6fbeebae25ed4132f57d7d2fca96f42f4e54347a
taalib-zama <taalib.zama@gmail.com>
2026-02-26 01:33:52 +0530

    Updating project with frinend app

--- springBasicSamples/electronicStorePorject/frontend-app-electrinic-store/google-login-app/src/app/app.config.ts
+++ springBasicSamples/electronicStorePorject/frontend-app-electrinic-store/google-login-app/src/app/app.config.ts
@@ -2,7 +2,32 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { GoogleLoginProvider, SOCIAL_AUTH_CONFIG, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import { provideHttpClient } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+
+  providers: [
+    provideHttpClient(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: SOCIAL_AUTH_CONFIG,
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '843272962480-co6tjq63a6ocp96ekuegimr1p1s0es69.apps.googleusercontent.com'
+            )
+          }
+        ],
+        onError: (err: any) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig
+    }
+  ]
 };
